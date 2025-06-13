4ecf4745e04c0be6b0f7aeb32c83618ebf765d12
Michal <14275273+Michal-Koeckeis-Fresel@users.noreply.github.com>
2025-06-13 12:15:17 +0200

    Delete linux/cloudinit/history-config.sh

--- linux/cloudinit/history-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Author: Michal Koeckeis-Fresel
-# License: MIT
-# Bash history configuration for improved command logging
-
-PROMPT_COMMAND='history -a'
-HISTTIMEFORMAT="%F %T "
-HISTSIZE=11000
-HISTFILESIZE=11000
